3dbb187e58641c5366f5f0c870aee5c7512985ad
Jaden Jang <jaden.jang@compax.at>
2025-01-09 17:44:23 +0900

    Initial commit

---
+++ src/main/java/me/blog/owlblogbackend/OwlBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package me.blog.owlblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlBlogBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OwlBlogBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/blog/owlblogbackend/OwlBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.blog.owlblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlBlogBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
