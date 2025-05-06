cf11fe6c4c14fb6b6cacc50d5f38d4284e61998e
jinsujj <wlstncjs1234@naver.com>
2025-05-06 13:30:44 +0900

    Swagger enabled 수동 설정

--- src/main/java/me/blog/backend/bounded/context/blog/adapter/in/api/WelcomeController.java
+++ src/main/java/me/blog/backend/bounded/context/blog/adapter/in/api/WelcomeController.java
@@ -1,9 +1,11 @@
 package me.blog.backend.bounded.context.blog.adapter.in.api;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Profile("!prod")
 public class WelcomeController {
 
   @GetMapping
