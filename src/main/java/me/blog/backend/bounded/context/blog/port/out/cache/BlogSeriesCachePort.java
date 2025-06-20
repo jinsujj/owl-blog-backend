03abc2a47cfd1237a37511edd31d5103e9eab56d
jinsujj <wlstncjs1234@naver.com>
2025-06-20 22:51:23 +0900

    delete unused import

--- src/main/java/me/blog/backend/bounded/context/blog/port/out/cache/BlogSeriesCachePort.java
+++ src/main/java/me/blog/backend/bounded/context/blog/port/out/cache/BlogSeriesCachePort.java
@@ -1,12 +1,8 @@
 package me.blog.backend.bounded.context.blog.port.out.cache;
 
-import me.blog.backend.bounded.context.blog.domain.model.BlogEntity;
-import me.blog.backend.bounded.context.blog.domain.model.BlogSeriesEntity;
-import me.blog.backend.bounded.context.blog.domain.model.SeriesEntity;
 import me.blog.backend.bounded.context.blog.domain.vo.BlogSeriesVO;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BlogSeriesCachePort {
     void putAll();
--- src/main/java/me/blog/backend/common/config/RedisConfig.java
+++ src/main/java/me/blog/backend/common/config/RedisConfig.java
@@ -4,17 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-import me.blog.backend.bounded.context.blog.domain.model.BlogSeriesEntity;
 import me.blog.backend.bounded.context.blog.domain.vo.BlogSeriesVO;
 import me.blog.backend.bounded.context.blog.domain.vo.BlogVO;
 
